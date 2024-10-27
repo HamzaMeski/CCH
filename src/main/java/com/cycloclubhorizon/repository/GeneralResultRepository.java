6a05a5e7682ee659b5782f39847d93a3722b0c77
hamzaMeski <meskihamza5@gmail.com>
2024-10-27 15:48:44 +0100

    update GeneralResultRepository

--- src/main/java/com/cycloclubhorizon/repository/GeneralResultRepository.java
+++ src/main/java/com/cycloclubhorizon/repository/GeneralResultRepository.java
@@ -4,7 +4,9 @@ import com.cycloclubhorizon.model.GeneralResult;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface GeneralResultRepository extends JpaRepository<GeneralResult, Long> {
-    // Custom query methods can be added here
+    Optional<GeneralResult> findByCompetitionIdAndCyclistId(Long competitionId, Long cyclistId);
 }
