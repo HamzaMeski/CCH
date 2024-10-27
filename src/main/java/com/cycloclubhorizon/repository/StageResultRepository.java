b4e90c521ccee462df4fc5f442f5e57f712c557d
hamzaMeski <meskihamza5@gmail.com>
2024-10-27 15:58:08 +0100

    Update StageResultRepository

--- src/main/java/com/cycloclubhorizon/repository/StageResultRepository.java
+++ src/main/java/com/cycloclubhorizon/repository/StageResultRepository.java
@@ -4,7 +4,9 @@ import com.cycloclubhorizon.model.StageResult;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface StageResultRepository extends JpaRepository<StageResult, Long> {
-    // Custom query methods can be added here
+    Optional<StageResult> findByStageIdAndCyclistId(Long stageId, Long cyclistId);
 }
